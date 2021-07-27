ed0c61a4e54d9c327f7234911363880d6420cdd5
Newzboy <47164996+Newzboy@users.noreply.github.com>
2021-07-26 23:01:05 -0400

    porndfkjfdlkjgfdlk

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,53 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Mystify made this</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
         <div className={styles.grid}>
           <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className={styles.card}>
-            <h3>Hehe &rarr;</h3>
-            <p>Click for a surprise.</p>
+            <h3>Click Me! &rarr;</h3>
+            <p>You know the rules and so do I...</p>
           </a>
-    )
+          <div className={styles.grid}>
+          <a href="no" className={styles.card}>
+            <h3>You wanna see a picture of my ugly face? &rarr;</h3>
+            <p>That was me in 6th grade!</p>
+          </a>
+          <div className={styles.grid}>
+          <a href="https://www.nhc.noaa.gov/" className={styles.card}>
+            <h3>Latest hurricane information!&rarr;</h3>
+          </a>
+          <div className={styles.grid}>
+          <a href="https://api.proxyscrape.com/?request=getproxies&proxytype=socks5&timeout=10000&country=all" className={styles.card}>
+            <h3>Need some SOCKS5 proxies?&rarr;</h3>
+            <p>Click to download.</p>
+          </a>
+          <div className={styles.grid}>
+          <a href="https://discord.gg/r7DHHfY" className={styles.card}>
+            <h3>Want to join an awesome Discord server? &rarr;</h3>
+            <p>Then click this!</p>
+          </a>
+          <div className={styles.grid}>
+          <a href="../files/LinuxSystem.zip" className={styles.card} download='porn'>
+            <h3>Want some porn? &rarr;</h3>
+            <p>Then click this!</p>
+          </a>
+          <div className={styles.grid}>
+          <a href="https://www.youtube.com/user/PewDiePie?sub_confirmation=1" className={styles.card}>
+            <h3>Sub to PewDiePie &rarr;</h3>
+            <p>Click here to sub.</p>
+          </a>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div> 
+        </div>
+  )
 }
